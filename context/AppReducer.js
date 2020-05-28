ed514f43535ab5017262f4fcdd2eb3f0205049ce
engineeringgeeksquad <engineering.geedsquad@gmail.com>
2020-05-28 20:26:31 +0500

    progressbar added

--- context/AppReducer.js
+++ context/AppReducer.js
@@ -11,13 +11,17 @@ export default (state, action) => {
                 id: action.payload
             }
         case "SET_LOADER":
-            return {...state, loader: action.payload};
+            return { ...state, loader: action.payload };
         case "SET_LOGGEDIN":
-            return {...state, loggedIn: action.payload};
+            return { ...state, loggedIn: action.payload };
         case "SET_MAIN_SCREEN":
-            return {...state, mainScreen: action.payload};
-            case "SET_HRM":
-                return {...state, hrm: action.payload};
+            return { ...state, mainScreen: action.payload };
+        case "SET_HRM":
+            return { ...state, hrm: action.payload };
+        case "SET_CONNECT_DEVICE":
+            return { ...state, connectDevice: action.payload };
+        case "SET_DEVICE_ID":
+            return { ...state, deviceId: action.payload };
         default:
             return state;
     }
--- screens/liveStatsScreen/index.js
+++ screens/liveStatsScreen/index.js
@@ -147,7 +147,8 @@ const styles = StyleSheet.create({
     backImage: {
         width: 16,
         height: 16,
-        resizeMode: 'contain'
+        resizeMode: 'contain',
+        marginBottom: '5%'
     },
 
     liveStatsTitleSection: {
